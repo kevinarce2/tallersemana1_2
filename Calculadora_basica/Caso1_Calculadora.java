7330f3e5b05a88988c5467c98ac3cfe7141c14a0
kevinarce2 <kevin.arce@opolarity.com>
2026-04-12 16:59:24 -0500

    tarea de campo culminada

--- Calculadora_basica/Caso1_Calculadora.java
+++ Calculadora_basica/Caso1_Calculadora.java
@@ -1,27 +1,19 @@
 import java.util.Scanner;
 
-/**
- * CASO 1: Calculadora básica
- * Permite realizar operaciones básicas entre dos números
- */
 public class Caso1_Calculadora {
 
-    // Método para sumar
     public static double sumar(double a, double b) {
         return a + b;
     }
 
-    // Método para restar
     public static double restar(double a, double b) {
         return a - b;
     }
 
-    // Método para multiplicar
     public static double multiplicar(double a, double b) {
         return a * b;
     }
 
-    // Método para dividir
     public static double dividir(double a, double b) {
         if (b == 0) {
             System.out.println("Error: No se puede dividir entre cero");
